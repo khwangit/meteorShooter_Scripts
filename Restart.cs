0ef3a6e31c6c6d84dd175219504bdb38db4c77f3
khwangit <wang.k@husky.neu.edu>
2017-05-04 18:33:04 -0400

    add credits scene

--- Restart.cs
+++ Restart.cs
@@ -6,4 +6,12 @@ public class Restart : MonoBehaviour {
 	public void RestartGame(){
 		SceneManager.LoadScene ("First", LoadSceneMode.Single);
 	}
+
+	public void BackToStart(){
+		SceneManager.LoadScene ("Start", LoadSceneMode.Single);
+	}
+
+	public void ToCredits(){
+		SceneManager.LoadScene ("Credits", LoadSceneMode.Single);
+	}
 }
